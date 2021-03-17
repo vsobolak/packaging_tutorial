5807b4b3025e80e080c2806fab0179b873fd7b1f
Valerian Sobolak <sobolak.valerian@gmail.com>
2021-03-17 16:28:51 +0100

    test

--- setup.py
+++ setup.py
@@ -1,2 +1,15 @@
+from setuptools import setup
+from biketrauma import __version__ as current_version
 
+setup(
+  name='biketrauma',
+  version=current_version,
+  description='Visualization of a bicycle accident db',
+  url='http://github.com/vsobolak.git',
+  author='vsobolak',
+  author_email='sobolak.valerian@xgmail.com',
+  license='MIT',
+  packages=['biketrauma','biketrauma.io', 'biketrauma.preprocess', 'biketrauma.vis'],
+  zip_safe=False
+)
 
